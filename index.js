8494d4a3a82b48b705e440d4dd52e04b8b7744ca
zanyarhussein <zanyarhusen95@gmail.com>
2025-08-15 13:22:13 +0300

    finishing the js

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-var tBox= document.querySelector(".text-box input");
+var tBox = document.querySelector(".text-box input");
 var eOp = document.querySelector("#equal");
 var cOp = document.querySelector(".clear");
 
-for(var i = 0; i < document.querySelectorAll("button").length; i++){
-    document.querySelectorAll("button")[i].addEventListener("click", function(){
-        if(this.innerText >= '0' && this.innerText <= '9'){
-            tBox.value += this.innerText;
-        }else if(this.innerText=== "+" ||this.innerText=== "*"||this.innerText=== "-"||this.innerText=== "/"){
-            tBox.value += this.innerText;
-        }
-    
-    })
-}
-eOp.addEventListener("click", function(){
-    tBox.value = eval(tBox.value);
-})
-cOp.addEventListener("click", function(){
-        tBox.value = tBox.value.slice(0, tBox.value.length - 1);
-    
-})
+tBox.value = "0";
 
+for (var i = 0; i < document.querySelectorAll("button").length; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        let lastChar = tBox.value.slice(-1);
+
+        if (this.innerText >= '0' && this.innerText <= '9') {
+            if (tBox.value === "0") {
+                tBox.value = this.innerText;
+            } else {
+                tBox.value += this.innerText;
+            }
+        }
+        else if (["+", "-", "*", "/"].includes(this.innerText)) {
+            if (tBox.value !== "0" && !["+", "-", "*", "/"].includes(lastChar)) {
+                tBox.value += this.innerText;
+            }
+        }
+    });
+}
+
+function calculate() {
+    let exp = tBox.value;
+
+    if (/[\+\-\*\/]$/.test(exp)) {
+        exp = exp.slice(0, -1);
+    }
+
+    try {
+        tBox.value = eval(exp);
+    } catch {
+        tBox.value = "0";
+    }
+}
+
+cOp.addEventListener("click", function () {
+    tBox.value = "";
+    if (tBox.value === "") {
+        tBox.value = "0";
+    }
+});
